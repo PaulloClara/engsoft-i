c78397ab92e21ecf6766f874c648553535e505e0
paulloclara <paulloclara@gmail.com>
2019-11-09 11:28:22 -0300

    refactor/feat(utils/tk): "GUI" to "TKUtils", unnecessary attributes removed and initial scrollframe added

--- src/utils/tk.py
+++ src/utils/tk.py
@@ -1,14 +1,12 @@
-from tkinter import *
+import tkinter as tk
 
 
-class GUI:
-    
+class TKUtils:
+
     def __init__(self):
-        self.Window = Tk
-        self.Container = Frame
-        self.Input = Entry
-        self.Button = Button
-        self.Label = Label
+        self.Window = tk.Tk
+        self.Container = tk.Frame
+        self.ScrollFrame = ScrollFrame
 
         self.__label_config = {}
         self.__label_config['text'] = '...'
@@ -23,16 +21,16 @@ class GUI:
         self.__button_config['padx'] = 6
         self.__button_config['font'] = ('arial', 14, 'bold')
 
-        self.__input_config = {}
-        self.__input_config['width'] = 20
-        self.__input_config['bg'] = "white"
-        self.__input_config['fg'] = "black"
-        self.__input_config['bd'] = 0
-        self.__input_config['justify'] = 'left'
-        self.__input_config['font'] = ('arial', 13, 'italic')
+        self.__entry_config = {}
+        self.__entry_config['width'] = 20
+        self.__entry_config['bg'] = "white"
+        self.__entry_config['fg'] = "black"
+        self.__entry_config['bd'] = 0
+        self.__entry_config['justify'] = 'left'
+        self.__entry_config['font'] = ('arial', 13, 'italic')
 
     def get_label(self, master, cnf={}, pack={}, grid={}):
-        label = self.Label(master=master, cnf=self.__label_config)
+        label = tk.Label(master=master, cnf=self.__label_config)
         label.configure(cnf=cnf)
 
         if grid:
@@ -43,7 +41,7 @@ class GUI:
         return label
 
     def get_button(self, master, cnf={}, pack={}, grid={}):
-        button = self.Button(master=master, cnf=self.__button_config)
+        button = tk.Button(master=master, cnf=self.__button_config)
         button.configure(cnf=cnf)
 
         if grid:
@@ -54,7 +52,7 @@ class GUI:
         return button
 
     def get_input(self, master, cnf={}, pack={}, grid={}):
-        _input = self.Input(master=master, cnf=self.__input_config)
+        _input = tk.Entry(master=master, cnf=self.__entry_config)
         _input.configure(cnf=cnf)
 
         if grid:
@@ -83,4 +81,36 @@ class GUI:
         return window
 
 
-UI = GUI()
+class ScrollFrame(tk.Frame):
+
+    def __init__(self, master, cnf={}, cs_cnf={}, vt_cnf={}, sr_cnf={}):
+        super().__init__(master=master, cnf=cnf)
+        self.pack(expand=True)
+
+        self.canvas = tk.Canvas(master=self, cnf=cs_cnf)
+        self.viewport = tk.Frame(master=self.canvas, cnf=vt_cnf)
+        self.scrollbar = tk.Scrollbar(
+            master=self, cnf=sr_cnf, command=self.canvas.yview)
+
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        self.scrollbar.pack(side='right', fill='y')
+        self.canvas.pack(side='left', fill='both', expand=True)
+        self.canvas_window =\
+            self.canvas.create_window(
+                (4, 4), window=self.viewport, anchor='nw', tags='self.viewport')
+
+        self.viewport.bind('<Configure>', self.on_frame_configure)
+        self.canvas.bind('<Configure>', self.on_canvas_configure)
+
+        self.on_frame_configure(None)
+
+    def on_frame_configure(self, event):
+        self.canvas.configure(scrollregion=self.canvas.bbox('all'))
+
+    def on_canvas_configure(self, event):
+        canvas_width = event.width
+        self.canvas.itemconfig(self.canvas_window, width=canvas_width)
+
+
+UI = TKUtils()
