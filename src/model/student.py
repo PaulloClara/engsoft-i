3a03175b76c5889b04523248880a227153251f82
paulloclara <paulloclara@gmail.com>
2019-11-12 10:24:31 -0300

    feat(model): read student names in csv file

---
+++ src/model/student.py
@@ -0,0 +1,20 @@
+class Student:
+
+    def __init__(self, model, controller):
+        self.__model = model
+        self.__controller = controller
+
+        self.students = []
+
+        self.read_file()
+
+    def read_file(self):
+        file = open("src/store/student.csv", mode="r")
+
+        for line in file:
+            self.students.append(self.clear_line(line=line))
+
+        file.close()
+
+    def clear_line(self, line):
+        return line.replace('\n', '')
