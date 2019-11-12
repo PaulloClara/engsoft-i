8a4b14fbd14052f5c0b246783b621680634881c2
paulloclara <paulloclara@gmail.com>
2019-11-12 10:22:56 -0300

    feat(view): initial raffle window

---
+++ src/view/raffle_window.py
@@ -0,0 +1,48 @@
+from src.utils.tk import UI
+
+
+class RaffleWindow(UI.Window):
+
+    def __init__(self, student, activity):
+        super().__init__()
+
+        self.title('Grande Felizardo(a)')
+        self.geometry(f'{len(student) * 16}x100')
+        self.resizable(0, 0)
+
+        self.student_name = student
+        self.activity_title = activity
+
+        self.container = None
+        self.student_label = None
+        self.activity_label = None
+        self.confirm_button = None
+
+        self._create_container()
+        self._create_student_label()
+        self._create_activity_label()
+
+    def _create_container(self):
+        self.container = UI.get_container(master=self)
+
+    def _create_student_label(self):
+        cnf, pack = {}, {}
+
+        cnf['text'] = f'Aluno: {self.student_name}'
+        cnf['bd'] = 4
+        cnf['fg'] = 'red'
+        cnf['font'] = ('arial', 16, 'bold')
+
+        self.student_label =\
+            UI.get_label(master=self.container, cnf=cnf, pack=pack)
+
+    def _create_activity_label(self):
+        cnf, pack = {}, {}
+
+        cnf['text'] = f'Atividade: {self.activity_title}'
+        cnf['bd'] = 4
+        cnf['fg'] = 'blue'
+        cnf['font'] = ('arial', 16, 'bold')
+
+        self.activity_label =\
+            UI.get_label(master=self.container, cnf=cnf, pack=pack)
