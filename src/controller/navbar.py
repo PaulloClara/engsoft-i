216fb3992b3291cc4b720e05efb0dab0a2e86f5d
paulloclara <paulloclara@gmail.com>
2020-01-05 12:36:45 -0300

    refactor: metodos desnecessarios

--- src/controller/navbar.py
+++ src/controller/navbar.py
@@ -14,53 +14,25 @@ class Navbar(object):
 
         self.configurar()
 
-    def tela_home(self, evt: object or None=None) -> None:
-        """Evento click do botao home."""
-        if self.view.container_ativo == 'home':
+    def ativar_(self, evt, elemento: str):
+        if self.view.container_ativo == elemento:
             return
 
         self.view.ocultar_container_ativo()
-        self.view.mostrar_container('home')
-
-    def tela_aluno(self, evt: object or None=None) -> None:
-        """Evento click do botao aluno."""
-        if self.view.container_ativo == 'aluno':
-            return
-
-        self.view.ocultar_container_ativo()
-        self.view.mostrar_container('aluno')
-
-    def tela_atividade(self, evt: object or None=None) -> None:
-        """Evento click do botao atividade."""
-        if self.view.container_ativo == 'atividade':
-            return
-
-        self.view.ocultar_container_ativo()
-        self.view.mostrar_container('atividade')
-
-    def tela_grupo(self, evt: object or None=None) -> None:
-        """Evento click do botao grupo."""
-        if self.view.container_ativo == 'grupo':
-            return
-
-        self.view.ocultar_container_ativo()
-        self.view.mostrar_container('grupo')
-
-    def tela_sobre(self, evt: object or None=None) -> None:
-        """Evento click do botao sobre."""
-        if self.view.container_ativo == 'sobre':
-            return
-
-        self.view.ocultar_container_ativo()
-        self.view.mostrar_container('sobre')
+        self.view.mostrar_container(elemento)
 
     def configurar(self) -> None:
         navbar = self.view.navbar
 
-        navbar.subelemento.home.evento['<Button-1>'] = self.tela_home
-        navbar.subelemento.aluno.evento['<Button-1>'] = self.tela_aluno
-        navbar.subelemento.grupo.evento['<Button-1>'] = self.tela_grupo
-        navbar.subelemento.sobre.evento['<Button-1>'] = self.tela_sobre
-        navbar.subelemento.atividade.evento['<Button-1>'] = self.tela_atividade
+        navbar.subelemento.home.evento['<Button-1>'] =\
+            lambda evt: self.ativar_(evt, 'home')
+        navbar.subelemento.aluno.evento['<Button-1>'] =\
+            lambda evt: self.ativar_(evt, 'aluno')
+        navbar.subelemento.grupo.evento['<Button-1>'] =\
+            lambda evt: self.ativar_(evt, 'grupo')
+        navbar.subelemento.sobre.evento['<Button-1>'] =\
+            lambda evt: self.ativar_(evt, 'sobre')
+        navbar.subelemento.atividade.evento['<Button-1>'] =\
+            lambda evt: self.ativar_(evt, 'atividade')
 
         navbar.carregar_eventos()
