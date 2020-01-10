5365943a076275e40a41129d808e62da69b667ee
paulloclara <paulloclara@gmail.com>
2020-01-10 16:04:08 -0300

    feat: add metodos para datas

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,7 +1,7 @@
 """Conjunto de codigo reaproveitavel."""
 
 from random import randint
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class Utils(object):
@@ -16,3 +16,20 @@ class Utils(object):
     def data_e_hora_atual() -> str:
         """Devolve uma data em string no formato: dd/mm/aaaa hh:mm."""
         return datetime.now().strftime('%d/%m/%Y %H:%M')
+
+    @staticmethod
+    def data_e_hora_em_(dias: int) -> str:
+        """Devolve uma data em string no formato: dd/mm/aaaa hh:mm + dd."""
+        data_e_hora = datetime.now() + timedelta(days=dias)
+
+        return data_e_hora.strftime('%d/%m/%Y %H:%M')
+
+    @staticmethod
+    def comparar_(data1: str, data2: str) -> bool or None:
+        data1 = ''.join(data1.split('/')[::-1])
+        data2 = ''.join(data2.split('/')[::-1])
+
+        if data1 > data2:
+            return True
+        if data1 < data2:
+            return False
